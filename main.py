8bf6ee259b5061396f8b7049e5fd6c6efedaf8a2
Рената Устинова <minrenata@bk.ru>
2022-11-24 12:42:09 +0300

    Добавлены изменения в документации.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from graphic_arts.start_game_banner import run_screensaver
 
 
 def attack(char_name: str, char_class: str) -> str:
-    """Определяется размер нанесенного урона после атаки для каждого класса."""
+    """Определяет размер нанесенного урона после атаки."""
     if char_class == 'warrior':
         return (f'{char_name} нанёс урон противнику равный'
                 f' {5 + randint(3, 5)}')
